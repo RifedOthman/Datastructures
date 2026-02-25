f3825c7623c54fafc2d8467ccf46f3bf2fc4178f
Rifed17 <rifed1704@gmail.com>
2026-02-25 20:58:01 +0100

    romantoint

---
+++ src/RomanToInteger/Main.java
@@ -0,0 +1,12 @@
+package RomanToInteger;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        RomanToInteger obj = new RomanToInteger();
+        int result = obj.romantoint("VIIII") ;
+        System.out.println(result);
+    }
+
+}
